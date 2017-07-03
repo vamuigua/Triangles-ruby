6ff6e49fa8ea8d458791f1edad48e3401f1bbfb2
Victor Allen <vamuigua@gmail.com>
2017-07-03 14:32:18 +0300

    Added method to check if triangle is equilateral

---
+++ lib/triangle.rb
@@ -0,0 +1,14 @@
+class Triangle
+    define_method(:initialize) do |side_1, side_2, side_3|
+        @side_1 = side_1
+        @side_2 = side_2
+        @side_3 = side_3
+    end
+    
+    #method for checking type of Triangle
+    define_method(:type?) do
+        if ((@side_1.eql?(@side_2)) && (@side_1.eql?(@side_3)) && (@side_2.eql?(@side_3)))
+        "equilateral"
+    end
+end
+end
