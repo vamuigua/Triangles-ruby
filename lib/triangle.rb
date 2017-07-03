e02c34be669af4052c3f1e890849e02509f9a27e
Victor Allen <vamuigua@gmail.com>
2017-07-03 14:57:49 +0300

    Added method to check if the triangle is a scalene

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,8 +11,11 @@ class Triangle
         if ((@side_1.eql?(@side_2)) && (@side_1.eql?(@side_3)) && (@side_2.eql?(@side_3)))
             "equilateral"
         #checks if its isosceles
-        elsif((@side_1.eql?(@side_2)) || (@side_1.eql?(@side_3)) || (@side_3.eql?(@side_2)))
+        elsif ((@side_1.eql?(@side_2)) || (@side_1.eql?(@side_3)) || (@side_3.eql?(@side_2)))
             "isosceles"
+        #checks if its a scalene
+        elsif (@side_1 != @side_2) && (@side_1 != @side_3) && (@side_3 != @side_2)
+            "scalene"
+        end
     end
-end
 end
